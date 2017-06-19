7ea8efcabbdd7f1bc86dcd9f87a0d1fa36eab545
Adrian S <4drian.s.m@gmail.com>
2017-06-19 00:15:01 -0500

    DB r/w + btn arregados

--- js/webSQL.js
+++ js/webSQL.js
@@ -2,12 +2,12 @@ var db = null;
 LoadDB();
 
 function LoadDB () {
-  db = openDatabase('mainDB', '0.1', 'no descrption', 5 * 1024 * 1024);
+  db = openDatabase('DB', '0.1', 'no descrption', 5 * 1024 * 1024);
   if(!db){
     console.log('error al crear db')
   }else{
     db.transaction(function (tx) {
-    tx.executeSql('CREATE TABLE IF NOT EXISTS Datos(usuario, sitio, contraseña)');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS Datos (usuario, sitio, contraseña)');
     });
   }
 }
@@ -22,20 +22,31 @@ function add () {
 }
 
 function read () {
-  db.readTransaction( function(transaction) {
-    transaction.executeSql('Select * from Datos;', [], function(transaction, result){
+  db.readTransaction( function(tx) {
+    tx.executeSql('Select * from Datos;', [], function(transaction, result){
     
-    for (var i=0; i < result.rows.length; i++) { 
-      var row = result.rows.item(i);
-      var msg =  "<tr><td>" + row.sitio + "</td>" + "<td>" + row.usuario + "</td>" +
-                 "<td>" + row.contraseña + "</td>" +
-                 `<td><label><input type="checkbox" name=""id=""</label></td></tr>` ;
-      document.getElementById('tb-status').innerHTML += msg;
-    /*console.log(row.sitio); 
-      console.log(row.usuario); 
-      console.log(row.contraseña);*/ 
-    } 
-  }); 
-  } 
-  );
+      for (var i=0; i < result.rows.length; i++) { 
+        var row = result.rows.item(i);
+        var msg =  '<tr><td>' + row.sitio + "</td>" + "<td>" + row.usuario + "</td>" +
+                   "<td>" + row.contraseña + "</td>" +
+                   `<td><label><input type="checkbox" id="del-check"</label></td></tr>` ;
+        document.getElementById('tb-status').innerHTML += msg;
+      /*console.log(row.sitio); 
+        console.log(row.usuario); 
+        console.log(row.contraseña);*/ 
+      } 
+    }); 
+  });
+}
+function clean () {
+  db.transaction( function(tx) {
+  tx.executeSql("DELETE FROM Datos", []);
+  });
+}
+function sup () {
+  db.transaction( function(tx) {
+  tx.executeSql('DELETE FROM Datos WHERE usuario=?;', [Adrian], function (tx, error) {
+    console.log('error borrar: ' + error.message)    
+  });
+  });
 }

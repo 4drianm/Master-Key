675d8c38a23a8612f94383251819b42d2bb94ae2
Adrian S <4drian.s.m@gmail.com>
2017-06-22 01:58:38 -0500

    retoques en interfaz

---
+++ js/key-gen.js
@@ -0,0 +1,12 @@
+function generar(longitud){
+
+	long=parseInt(longitud);
+	var caracteres = "abcdefghijkmnpqrtuvwxyzABCDEFGHIJKLMNPQRTUVWXYZ2346789!@#$¬&/“(”)=´?‚¿¡^`+–"
+	var contraseña = "";
+	for (i=0; i<long; i++) 
+		contraseña += caracteres.charAt(Math.floor(Math.random()*caracteres.length));	
+	var pass = `<label>Contraseña:</label> ` +
+							`<input type="text" class="form-control" id="contraseña" value="`+contraseña+`">`;
+	document.getElementById("pass").innerHTML=pass;
+
+}

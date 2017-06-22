78ebfbba6f0f226e79f05a31f0fa1b38b0b7954e
Adrian S <4drian.s.m@gmail.com>
2017-06-22 14:29:18 -0500

    timeout app

--- js/key-gen.js
+++ js/key-gen.js
@@ -1,12 +1,16 @@
 function generar(longitud){
 
 	long=parseInt(longitud);
-	var caracteres = "abcdefghijkmnpqrtuvwxyzABCDEFGHIJKLMNPQRTUVWXYZ2346789!@#$¬&/“(”)=´?‚¿¡^`+–"
+	var caracteres = "abcdefghijkmnpqrtuvwxyzABCDEFGHIJKLMNPQRTUVWXYZ2346789!@#$¬&/(”)=-?‚¿¡^+"
 	var contraseña = "";
 	for (i=0; i<long; i++) 
 		contraseña += caracteres.charAt(Math.floor(Math.random()*caracteres.length));	
 	var pass = `<label>Contraseña:</label> ` +
-							`<input type="text" class="form-control" id="contraseña" value="`+contraseña+`">`;
+							`<input type="text" class="form-control" id="contraseña" value="`+contraseña+`" readonly>`;
 	document.getElementById("pass").innerHTML=pass;
 
+}
+
+function timeout () {
+	location.href = "index.html";
 }
